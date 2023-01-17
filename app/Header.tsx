e3199a1162c0db5bcf567d044cd36a5df76730b1
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-17 21:55:00 +0100

    added second link to header, added layout for todos page to show todo next to todos list

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 function Header() {
 	return (
-		<header className="p-4 bg-blue-500">
+		<header className="p-4 bg-blue-500 flex gap-4">
 			<Link href="/" className="px-2 py-1 bg-white text-blue-500 rounded-lg">
 				Go to Home
+			</Link>
+			<Link href="/todos" className="px-2 py-1 bg-white text-blue-500 rounded-lg">
+				Go to Todos
 			</Link>
 		</header>
 	);
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import TodosList from './TodosList';
 
 function Todos() {
 	return (
 		<div>
-			{/* @ts-ignore */}
-			<TodosList />
+			<h1>This is where Todos will be listed...</h1>
 		</div>
 	);
 }
