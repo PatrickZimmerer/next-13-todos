3ae6aa55f35a3a71d940579c5511886469e10454
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-20 13:31:41 +0100

    added random timeout to test out suspense boundaries

--- app/(user)/todos/TodosList.tsx
+++ app/(user)/todos/TodosList.tsx
@@ -3,6 +3,9 @@ import { Todo } from '../../../typings';
 import Link from 'next/link';
 
 async function fetchTodos() {
+	const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+	await new Promise((resolve) => setTimeout(resolve, timeout));
+
 	const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
 	const todos: Todo[] = await res.json();
 	return todos;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import TodosList from './(user)/todos/TodosList';
 
 function Home() {
-	return <div className="from-black">Welcome home</div>;
+	return (
+		<div className="flex gap-12">
+			<Suspense fallback={<p className="text-green-500">Loading the Todos...</p>}>
+				<div className="flex flex-col">
+					{/* @ts-ignore */}
+					<TodosList />
+				</div>
+			</Suspense>
+			<Suspense
+				fallback={
+					<p className="text-red-500">Loading the other Todos with a different timeout...</p>
+				}
+			>
+				<div className="flex flex-col">
+					{/* @ts-ignore */}
+					<TodosList />
+				</div>
+			</Suspense>
+		</div>
+	);
 }
 
 export default Home;
