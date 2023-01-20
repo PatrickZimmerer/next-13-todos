7c1b9ad4a606b2d0ec16d8404676352e5b91a4db
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-20 13:09:26 +0100

    added app name to the head as well as a new app name when routing to the search page added error component if search fails

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import Header from './Header';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html>
-			<head></head>
+			<head>
+				<title>Patricks First Next13 App</title>
+			</head>
 			<body>
 				<Header />
 				{children}
