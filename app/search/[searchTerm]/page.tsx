d30df9094a3ac8895e815e681b4d4d396006087a
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-01-20 13:07:30 +0100

    added search function through SerpApi aswell as a loading component for searches

---
+++ app/search/[searchTerm]/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type PageProps = {
+	params: {
+		searchTerm: string;
+	};
+};
+
+async function search(searchTerm: string) {
+	const res = await fetch(
+		`https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
+	);
+	const data = await res.json();
+	return data;
+}
+
+async function SearchResults({ params: { searchTerm } }: PageProps) {
+	const searchResults = await search(searchTerm);
+	return (
+		<div>
+			<p className="text-gray-500 text-sm">You searched for: {searchTerm} </p>
+			<ol className="space-y-5 p-5">
+				{searchResults.organic_results.map((result: any) => (
+					<li key={result.position} className="list-decimal">
+						<p>{result.title}</p>
+						<p>{result.snippet}</p>
+					</li>
+				))}
+			</ol>
+		</div>
+	);
+}
+
+export default SearchResults;
